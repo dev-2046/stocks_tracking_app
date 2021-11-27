0b9a71a22e714aa367641c00ca9b092eab715fd8
dev-2046 <xifeng.jin@harrys.com>
2021-11-26 22:38:13 -0500

    add remove functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,11 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
   def self.new_lookup(ticker_symbol)
+    ticker = ticker_symbol.downcase
+    
     client =
       IEX::Api::Client.new(
         publishable_token: Rails.application.credentials.iex[:sandbox_api_key],
@@ -8,12 +14,16 @@ class Stock < ApplicationRecord
 
     begin
       new(
-        ticker: ticker_symbol,
-        name: client.company(ticker_symbol).company_name,
-        last_price: client.price(ticker_symbol)
+        ticker: ticker,
+        name: client.company(ticker).company_name,
+        last_price: client.price(ticker)
       )
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker)
+    where(ticker: ticker).first
   end
 end
